c4a765939b3fed558a158523b37896c90d460017
Matheus Hora <matheus11hora@gmail.com>
2023-05-24 21:09:54 -0300

    Novos icones

--- meu-portifolio/src/components/skills.tsx
+++ meu-portifolio/src/components/skills.tsx
@@ -2,18 +2,19 @@ import styles from "../style/skills.module.css";
 import flutterSvg from "../../public/tech/flutter.svg";
 import Image from "next/image";
 
-// import htmlIcon from "../public/tech/html.svg";
-// import cssIcon from "../public/tech/css.svg";
 import jsIcon from "../../public/tech/javascript.svg";
 import tyIcon from "../../public/tech/typescript.svg";
 import nextjsIcon from "../../public/tech/nextjs.svg";
-// import reactIcon from "../../public/tech/react.svg";
-// import nodejsIcon from "../../public/tech/nodejs.svg";
-// import mongoIcon from "../public/tech/mongo.svg";
-// import mysqlIcon from "../public/tech/mysql.svg";
-// import figmaIcon from "../public/figma.svg";
-// import photoshopIcon from "../public/photoshop.svg";
-// import illustratorIcon from "../public/illustrator.svg";
+import nodejsIcon from "../../public/tech/nodejs.svg";
+import androidIcon from "../../public/tech/android.svg";
+import kotlinIcon from "../../public/tech/kotlin.svg";
+import htmlIcon from "../../public/tech/html.svg";
+import cssIcon from "../../public/tech/css.svg";
+import pgIcon from "../../public/tech/pg.svg";
+import mysqlIcon from "../../public/tech/mysql.svg";
+import mariaIcon from "../../public/tech/mariadb.svg";
+import faribaseIcon from "../../public/tech/firebase.svg";
+import gitIcon from "../../public/tech/git.svg";
 
 function SkillsSection() {
   return (
@@ -24,17 +25,16 @@ function SkillsSection() {
         <Image src={jsIcon} width={50} height={50} alt="JavaScript" />
         <Image src={tyIcon} width={50} height={50} alt="TypeScipt" />
         <Image src={nextjsIcon} width={50} height={50} alt="Next.js" />
-        <Image src={flutterSvg} width={50} height={50} alt="Node.js" />
-        <Image src={flutterSvg} width={50} height={50} alt="Android" />
-        <Image src={flutterSvg} width={50} height={50} alt="Kotlin" />
-        <Image src={flutterSvg} width={50} height={50} alt="HTML" />
-        <Image src={flutterSvg} width={50} height={50} alt="Css" />
-        <Image src={flutterSvg} width={50} height={50} alt="Postgress" />
-        <Image src={flutterSvg} width={50} height={50} alt="Mysql" />
-        <Image src={flutterSvg} width={50} height={50} alt="MariaDB" />
-        <Image src={flutterSvg} width={50} height={50} alt="Faribase" />
-        <Image src={flutterSvg} width={50} height={50} alt="Git" />
-        <Image src={flutterSvg} width={50} height={50} alt="Figma" />
+        <Image src={nodejsIcon} width={50} height={50} alt="Node.js" />
+        <Image src={androidIcon} width={50} height={50} alt="Android" />
+        <Image src={kotlinIcon} width={50} height={50} alt="Kotlin" />
+        <Image src={htmlIcon} width={50} height={50} alt="HTML" />
+        <Image src={cssIcon} width={50} height={50} alt="Css" />
+        <Image src={pgIcon} width={50} height={50} alt="Postgress" />
+        <Image src={mysqlIcon} width={50} height={50} alt="Mysql" />
+        <Image src={mariaIcon} width={50} height={50} alt="MariaDB" />
+        <Image src={faribaseIcon} width={50} height={50} alt="Faribase" />
+        <Image src={gitIcon} width={50} height={50} alt="Git" />
       </div>
     </div>
   );
